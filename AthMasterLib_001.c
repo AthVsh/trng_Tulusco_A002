268d664b1446afa966cf5a3bb120011394b19bcc
AthVsh <amruth.kv@hotmail.com>
2018-01-08 14:56:29 +0000

    Create AthMasterLib_001.c

---
+++ AthMasterLib_001.c
@@ -0,0 +1,22 @@
+
+////////// Master: AthMasterLib_001.c 
+
+////////// Function-A001 /////////////////////////
+long double func_A001_add(double a, double b) 
+{   
+   long double result;   
+   result = (long double) (a) +  (long double) (b);    
+   return result;  
+}  
+
+////////// Function-A002 /////////////////////////
+long double func_A002_sub(double a, double b) 
+{   
+   long double result;    
+   result = (long double) (a) -  (long double) (b);      
+   return result;  
+} 
+
+//////////////////////////////////////////////////
+//|||||||||||||||||||||||||||||||||||||||||||||||
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
